682ad226bd2f3dae8d0a80c8f6f24f6896098bfe
kimbomin <tbmom13@naver.com>
2016-04-03 20:58:19 +0900

    Added files via upload

---
+++ w6main.py
@@ -0,0 +1,26 @@
+﻿import turtle
+window=turtle.Screen()
+A=raw_input('A input scissor, rock, paper:')
+B=raw_input('B input scissor, rock, paper:')
+if A=='scissor':
+    if B=='rock':
+        print "%s" %"B win"
+    elif B=='paper':
+        print "%s" %"A win"
+    else:
+        print "%s" %"draw"
+elif A=='rock':
+    if B=='scissor':
+        print "%s" %"A win"
+    elif B=='paper':
+        print "%s" %"B win"
+    else:
+        print "%s" %"draw"
+elif A=='paper':
+    if B=='scissor':
+        print "%s" %"B win"
+    elif B=='rock':
+        print "%s" %"A win"
+    else:
+        print "%s" %"draw"
+window.exitonclick()
