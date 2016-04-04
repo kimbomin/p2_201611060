f9a4f89d9f930dd5a555d502abec21f79ae9ec1e
kimbomin <tbmom13@naver.com>
2016-04-04 20:54:35 +0900

    bmi
    dd

---
+++ w5main(4).py
@@ -0,0 +1,14 @@
+﻿height=input ("input your height:")
+weight=input ("input your weight:")
+bmi=weight/(height*height)
+print "%s" %bmi
+if bmi<18.5:
+    print 'low weight'
+elif 18.5<= bmi <25:
+    print 'normal weight'
+elif 25<= bmi <30:
+    print 'overweight'
+elif 30<= bmi <40:
+    print 'obesity'
+else:
+    print 'seriously obese'
