28754eaf68b782c0ee45fcac75a15185959bd36b
kimbomin <tbmom13@naver.com>
2016-04-06 13:47:04 +0900

    6주차 과제
    아아아아아

---
+++ w6main.py
@@ -0,0 +1,15 @@
+﻿def 1000():
+    sum=0 
+    for i in range (1,1000):
+      if i % 3 == 0 or i % 5 == 0:
+          sum += i
+    print sum
+
+def lab6()
+    1000()
+
+def main():
+    lab6()
+
+if __name__=="__main__":
+    main()
