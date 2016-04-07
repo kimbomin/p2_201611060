fd5ce129ec85ea65f75e15edec457be9a7419b55
kimbomin <tbmom13@naver.com>
2016-04-07 15:54:53 +0900

    w6main
    dfdfdf

--- w6main.py
+++ w6main.py
@@ -5,9 +5,44 @@
           sum += i
     print sum
 
+def highlow():
+    import random
+
+    Noa = 0
+
+    print ('안녕! 이름이 뭐니?')
+    myName = raw_input()
+
+    number=random.randint(1,10)
+    print ('안녕, %s. 내가 1부터 10까지 숫자중에서 생각하고 있는걸 맞춰봐.') %myName
+
+    while Noa < 4:
+        print('물어봐.')
+        guess = input()
+        guess = int(guess)
+    
+        Noa = Noa + 1
+        if guess < number:
+            print('그거보단 높아.')
+    
+        if guess > number:
+            print('그거보단 낮아.')
+    
+        if guess == number:
+            break
+        
+    if guess == number:
+        Noa = str(Noa)
+        print('맞췄어! %s 번만에 맞췄네!') %Noa
+    
+    if guess != number:
+        number = str(number)
+        print('아쉽네..내가 생각한 숫자는 %s 야..') %number
+
 def lab6()
     1000()
-
+    highlow()
+    
 def main():
     lab6()
 
