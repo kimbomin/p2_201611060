d17d8c031f472a90f385ae58bd7c3710ca4c0cde
kimbomin <tbmom13@naver.com>
2016-04-11 19:34:18 +0900

    1 to 1000 sum
    1부터 1000까지 4로 나누어지고 5로 나누어지지 않는 수들의 합

---
+++ w6main(4)_1000sum.py
@@ -0,0 +1,21 @@
+﻿def sumList(aList):
+    x=list()
+    sum=0
+    for i in range(0,aList):
+        if i%4==0 and not i%5==0:
+            x.append(i)
+    mysum=0
+    for i in range(0,len(x)):
+        mysum += x[i]
+    return mysum
+
+def lab6():
+    """my homework"""
+    aList=1000
+    labsum=sumList(aList)
+    print labsum
+    
+def main():
+    lab6()
+    
+main()
