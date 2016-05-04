8f4aabed5b030d8a4266c97e22e261e1d93d1a79
kimbomin <tbmom13@naver.com>
2016-05-04 12:08:01 +0900

    hongjimun
    sdfadsf

---
+++ w9Main(2).py
@@ -0,0 +1,25 @@
+﻿def countDigitsLetters():
+    word='7 hongjimun'
+    allchars=list(word)         
+    d=dict()
+    d['number']=0
+    d['word']=0
+    for c in allchars:
+        if c.isdigit():
+            d['number']=d['number']+1
+        else:
+            d['word']=d['word']+1
+    import matplotlib
+    import matplotlib.pyplot as plt
+    plt.bar(range(len(d)),d.values(), align='center')
+    plt.xticks(range(len(d)), d.keys())
+    plt.show()
+
+def lab9():
+    countDigitsLetters()
+
+def main():
+    lab9()
+
+if __name__=="__main__":
+    main()
