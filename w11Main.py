2df4cbaca77534702147dce64b6500971481997c
kimbomin <tbmom13@naver.com>
2016-05-13 17:45:07 +0900

    speech and survey
    sdfafdsafdsa

--- w11Main.py
+++ w11Main.py
@@ -1,58 +1,108 @@
-﻿import turtle
-
-wn=turtle.Screen()
-
-t1=turtle.Turtle()
-
-t1.shape("turtle")
-
-t1.pencolor("green")
-
-def w():
-    t1.fd(50)
-
-def a():
-    t1.left(90)
-
-def s():
-    t1.back(50)
-
-def d():
-    t1.right(90)
-
-def addKeys():
-    wn.onkey(w,"Up")
-    wn.onkey(a,"Left")
-    wn.onkey(s,"Down")
-    wn.onkey(d,"Right")
+﻿def question(): 
+    dataA=[[13.1, 37.1], 
+          [10.6, 34.6], 
+          [27.1, 40.0], 
+          [16.2, 37.8], 
+          [11.4, 29.8], 
+          [12.2, 26.5], 
+          [13.5, 29.7], 
+          [13.7, 37.6]] 
+    dataB=[[8.7, 1.5], 
+          [13.4, 1.9], 
+          [2.9, 1.5], 
+          [6.8, 0.8], 
+          [14.8, 4.9], 
+          [14.9, 4.4], 
+          [11.1, 2.4], 
+          [4.1, 1.2]]
+ 
+    sumA=0 
+    sumB=0 
 
 
-def mousegoto(x,y):
-    t1.setpos(x,y)
-    (x,y)=t1.pos()
-    if 300<x<400 and 200<y<300:
-        print "YES YES YES!!"
+    for i in dataA: 
+        sumA=i[0]+i[1] 
+ 
+ 
+    for i in dataB: 
+        sumB=i[0]+i[1] 
+ 
+    print sumA
+    print sumB
+    print float(sumA/len(dataA))
+    print float(sumB/len(dataB))
 
-def addMouse():
-    wn.onclick(mousegoto)
+def speech():
+    Obama=["My fellow citizens.",
+    "I stand here today humbled by the task before us, grateful for the trust you have bestowed,",
+    "mindful of the sacrifices borne by our ancestors. I thank President Bush for his service to our nation,",
+    "as well as the generosity and cooperation he has shown throughout this transition.",
+    "Forty-four Americans have now taken the presidential oath.",
+    "The words have been spoken during rising tides of prosperity and the still waters of peace.",
+    "Yet, every so often the oath is taken amidst gathering clouds and raging storms.",
+    "At these moments, America has carried on not simply because of the skill or vision of those in high office,",
+    "but because We the People have remained faithful to the ideals of our forbearers,",
+    "and true to our founding documents."]
 
-addMouse()
+    def count():
+        d = {}
+        for i in Obama:
+            words=i.split()
+            for word in words:
+                if word in d:
+                    d[word]+=1
+                else:
+                    d[word]=1
+        return d
 
-addKeys()
+    def frequent(a,b):
+        wordss=list()
+        for i in b:
+            if b[i]>a:
+                wordss.append(i)
+        return wordss
 
-wn.listen()
+    wordsss=count()
+    frequentwords=frequent(8,wordsss)
+    print frequentwords
 
-def drawSquareAt(size, pos):
-    t1.penup()
-    t1.setpos(pos)
-    t1.pendown()
-    for i in range(0,4):
-        t1.forward(size)
-        t1.right(90)
+    Licoln=["Four score and seven years ago, our fathers brought forth on this continent,",
+    "a new nation, conceived in Liberty and dedicated to the proposition that all men are created equal.",
+    "Now we are engaged in a great civil war,",
+    "tesing whether that naion or any nation so conceived and so dedicated, can long endure.",
+    "It is rather for us to be here dedicated to the great task remaining before us ",
+    "that from these honored dead we take increased devotion to that cause for which they gave the last full measure of devotion",
+    "that we here highly resolve that these dead shall not have died in vain-that this nation, under God, shall have a new birth of freedom",
+    "and that government of the people, by the people, for the people, shall not perish from the earth."]
 
-drawSquareAt(100,(300,300))
+    def countL():
+        d = {}
+        for i in Licoln:
+            words=i.split()
+            for word in words:
+                if word in d:
+                    d[word]+=1
+                else:
+                    d[word]=1
+            return d
 
-t1.penup()
-t1.home()
+    def Lword(c,d):
+        lol=list()
+        for i in d:
+            if d[i]>c:
+                lol.append(i)
+        return lol
 
-turtle.mainloop()
+    word=countL()
+    Lwords=Lword(23,word)
+    print Lwords
+
+def lab11():
+    question()
+    speech()
+
+def main():
+    lab11()
+
+if __name__=="__main__":
+    main()
