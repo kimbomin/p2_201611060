f5d4fa000b68d9d3d90a48bd657dbfd399419957
kimbomin <tbmom13@naver.com>
2016-05-24 01:32:52 +0900

    python error and upper
    ddd

---
+++ w12Main(1).py
@@ -0,0 +1,12 @@
+﻿import os
+mydir=os.getcwd()
+filename='python.txt'
+myfilename=os.path.join(mydir,filename)
+try:
+    myfile=open(myfilename,'r')
+    for line in myfile:
+        if line.find('Python')>=0:
+            print line
+    myfile.close()
+except IOError as e:
+    print e
---
+++ w12Main(2).py
@@ -0,0 +1,20 @@
+﻿import os
+mydir=os.getcwd()
+filename='output.txt'
+myfilename=os.path.join(mydir,filename)
+myfile=open('output.txt','w')
+line1='first line\n'
+myfile.write(line1)
+line2='second line\n'
+myfile.write(line2)
+line3='third line'
+myfile.write(line3)
+myfile.close()
+myfile2=open(myfilename,'r')
+for line in myfile2:
+    if line.find('line')>=6:
+        print 'line'.upper()
+    if line.find('first' and 'second' and 'third')>=0:
+        print 'first'.lower()
+        print 'second'.lower()
+        print 'third'.lower()
