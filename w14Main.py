a4f9072cffdc7a392cb26aa81dd4a1f84dd70637
kimbomin <tbmom13@naver.com>
2016-06-03 18:16:54 +0900

    Bow - Wow
    멍멍

---
+++ w14Main.py
@@ -0,0 +1,29 @@
+﻿class Dog(object):
+    def __init__(self,name):
+        self.name=name
+    def getName(self):
+        print "my dog name is",self.name
+    def talk(self):
+        print "mung mung"
+class ShihTzuDog(Dog):
+    def talk(Dog):
+        print "si si"        
+class Maltese(Dog):
+    def talk(Dog):
+        print "mal mal"
+def Bowwow():
+	mydog=Dog('poppy')
+	mydog.talk()
+	myshihtzu=ShihTzuDog('poppy')
+	myshihtzu.talk()
+	mymaltese=Maltese('poppy')
+	mymaltese.talk()
+
+def lab14(): 
+    Bowwow() 
+     
+def main():    
+    lab14() 
+ 
+if __name__=="__main__": 
+    main() 
